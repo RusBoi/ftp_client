c4698a1d837167356f7a2d14219dd1f33fe951dd
RusBoi <kivixit@yandex.ru>
2016-10-13 11:28:25 +0500

    Bug fixes

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ except:
 import sys
 import time
 
+from shlex import split
 from socket import timeout
 
 DEFAULT_USERNAME = 'ftp'
@@ -18,26 +19,20 @@ TIMEOUT_CODE = 421
 DOWNLOAD_DEFAULT_PATH = './'
 
 
-class FTPClient:
-    def __init__(self):
-        self.handlers = {
-            'get': self.download_handler,
-            'put': self.upload_handler,
-            'user': self.user_handler,
-            'pwd': self.pwd_handler,
-            'rm': self.remove_handler,
-            'ren': self.rename_handler,
-            'cd': self.cd_handler,
-            'mkdir': self.mkdir_handler,
-            'ls': self.ls_handler,
-            'size': self.size_handler,
-            'debug': self.debug_handler,
-            'mode': self.switch_mode_handler,
-            'help': self.help_handler,
-            'exit': self.exit_handler,
-            None: self.unknown_command_handler
-        }
+def get_func(method: staticmethod):
+    def wrapper(args):
+        return method.__func__(args)
 
+    wrapper.__name__ = method.__func__.__name__
+    wrapper.__doc__ = method.__func__.__doc__
+    return wrapper
+
+
+class FTPClient:
+    ftp = None
+
+    @staticmethod
+    def parse_arguments():
         parser = argparse.ArgumentParser(description="""ftp client server""")
         parser.add_argument('host', help='host to connect to')
         parser.add_argument('--port', '-p', type=int, help='port to connect to', default=DEFAULT_PORT)
@@ -54,7 +49,8 @@ class FTPClient:
         parser_get = subparsers.add_parser('get', aliases=['download'], help='download file from the server')
         parser_get.add_argument('-r', action='store_true', help='download directory')
         parser_get.add_argument('path1', help='path where file located on the server')
-        parser_get.add_argument('path2', nargs='?', default=DOWNLOAD_DEFAULT_PATH, help='path where file will be stored on local machine')
+        parser_get.add_argument('path2', nargs='?', default=DOWNLOAD_DEFAULT_PATH,
+                                help='path where file will be stored on local machine')
         parser_get.set_defaults(func='get')
 
         parser_ls = subparsers.add_parser('ls', help='show content of remote directory')
@@ -65,20 +61,20 @@ class FTPClient:
 
         try:
             host = args.host or input('Enter host name: ')
-            self.ftp = ftplib.FTP(host, args.port, printout=print,
-                                  print_output=args.debug, print_input=not hasattr(args, 'func'))
+            FTPClient.ftp = ftplib.FTP(host, args.port, printout=print,
+                                       print_output=args.debug, print_input=not hasattr(args, 'func'))
         except KeyboardInterrupt:
             print()
             raise SystemExit
         except:
-            self.eprint(sys.exc_info()[1])
+            FTPClient.eprint(sys.exc_info()[1])
             raise SystemExit
 
         if args.user != DEFAULT_USERNAME and not args.passwd:
-            self.run_command('user', [args.user])
+            FTPClient.run_command('user', [args.user])
         else:
             password = args.passwd or DEFAULT_PASS
-            self.run_command('user', [args.user, password])
+            FTPClient.run_command('user', [args.user, password])
 
         if hasattr(args, 'func'):
             if args.func == 'ls':
@@ -88,50 +84,55 @@ class FTPClient:
             else:
                 a = [args.path1, args.path2]
 
-            self.run_command(args.func, a)
+            FTPClient.run_command(args.func, a)
             raise SystemExit
 
-    def run(self):
+    @staticmethod
+    def run():
         """
         Main cycle
         """
         while True:
             try:
-                command, *args = input('command: ').split(' ')
+                # command, *args = input('command: ').split(' ')
+                command, *args = split(input('command: '))
             except KeyboardInterrupt:
                 print()
-                self.exit_handler([])
+                FTPClient.exit_handler([])
             else:
-                self.run_command(command, args)
+                FTPClient.run_command(command, args)
 
-    def run_command(self, command, args):
+    @staticmethod
+    def run_command(command, args):
         """
         Sending(running) command to the server with handling exceptions
         :param command: command to run
         :param args: arguments
         """
-        if command not in self.handlers:
+        if command not in FTPClient.handlers:
             command = None
         try:
-            self.handlers[command](args)
+            FTPClient.handlers[command](args)
         except ftplib.WrongResponse as e:
             print('<<', e.response)
             if e.response.code == TIMEOUT_CODE:
                 # Если timeout то соединение восстановить не удастся и поэтому закроем клиент
-                self.exit_handler([])
+                FTPClient.exit_handler([])
         except ValueError:
-            self.eprint('Wrong arguments. Use "help <command>"')
+            FTPClient.eprint('Wrong arguments. Use "help <command>"')
         except KeyboardInterrupt:
             print()
             pass
         except timeout:
             print('Time out. Exiting program...')
-            self.exit_handler([])
+            FTPClient.exit_handler([])
         except ConnectionError:
             print(sys.exc_info()[1])
-            self.exit_handler([])
+            FTPClient.exit_handler([])
 
-    def download_handler(self, args):
+    @get_func
+    @staticmethod
+    def download_handler(args):
         """
         usage: get [-r] <path> [<directory_path>]
         Receive file from the server. If a file already exists then it will be overwritten.
@@ -151,51 +152,54 @@ class FTPClient:
             else:
                 path2 = os.path.expanduser(DOWNLOAD_DEFAULT_PATH)
             if not os.path.isdir(path2):
-                self.eprint('"{}" is not a directory'.format(path2))
+                FTPClient.eprint('"{}" is not a directory'.format(path2))
                 raise ValueError
-            path2 = path2.rstrip('/') + '/' + dirname
+
+            path2 = FTPClient.validate_path(path2.rstrip('/') + '/' + dirname)
+
             try:
                 os.mkdir(path2)
             except:
-                self.eprint(sys.exc_info()[1])
+                FTPClient.eprint(sys.exc_info()[1])
                 return
 
             # замьютим все сообщения
-            old_in, old_out = self.ftp.print_input, self.ftp.print_output
-            self.ftp.print_input = False
-            self.ftp.print_output = False
+            old_in, old_out = FTPClient.ftp.print_input, FTPClient.ftp.print_output
+            FTPClient.ftp.print_input = False
+            FTPClient.ftp.print_output = False
 
-            self.download_directory(path1, path2)
+            FTPClient.download_directory(path1, path2)
             # вернем сообщения обратно
-            self.ftp.print_input, self.ftp.print_output = old_in, old_out
+            FTPClient.ftp.print_input, FTPClient.ftp.print_output = old_in, old_out
         else:
             path1 = args[0].rstrip('/')
 
             path2 = args[1] if len(args) > 1 else DOWNLOAD_DEFAULT_PATH
             path2 = os.path.expanduser(path2)
             if not os.path.isdir(path2):
-                self.eprint('"{}" is not a directory'.format(path2))
+                FTPClient.eprint('"{}" is not a directory'.format(path2))
                 raise ValueError
             path2 = path2.rstrip('/') + '/' + os.path.split(path1)[-1]
             if os.path.isfile(path2):
                 os.remove(path2)
 
-            self.download_file(path1, path2)
+            FTPClient.download_file(path1, path2)
 
-    def download_file(self, path1, path2):
+    @staticmethod
+    def download_file(path1, path2):
         """
         :param path1: server file's path to the file
         :param path2: destination file's path on the local machine
         """
         bytes_count = 0
         start = time.time()
-        for data in self.ftp.download_file(path1):
+        for data in FTPClient.ftp.download_file(path1):
             bytes_count += len(data)
             try:
                 with open(path2, 'ab') as file:
                     file.write(data)
             except:
-                self.eprint(sys.exc_info()[1])
+                FTPClient.eprint(sys.exc_info()[1])
                 return
 
         result_time = time.time() - start
@@ -205,31 +209,34 @@ class FTPClient:
                                                                       speed)
         print(info_string)
 
-    def download_directory(self, current_path, dest_path):
+    @staticmethod
+    def download_directory(current_path, dest_path):
         """
         :param current_path: current server's path
         :param dest_path: current local path
         """
         try:
-            files = self.ftp.list_files(current_path)
+            files = FTPClient.ftp.list_files(current_path)
         except ftplib.WrongResponse as e:
-            self.eprint(e)
+            FTPClient.eprint(e)
             return
         for t in files:
             name = t[0]
-            server_path = '{}/{}'.format(current_path, name)
+            server_path = '{}/{}'.format(current_path.rstrip('/'), name)
             print(server_path)
             next_path = '{}/{}'.format(dest_path, name)
             if t[1]:
                 try:
-                    self.download_file(server_path, next_path)
+                    FTPClient.download_file(server_path, next_path)
                 except ftplib.WrongResponse as e:
-                    self.eprint(e)
+                    FTPClient.eprint(e)
             else:
                 os.mkdir(next_path)
-                self.download_directory(server_path, next_path)
+                FTPClient.download_directory(server_path, next_path)
 
-    def upload_handler(self, args):
+    @get_func
+    @staticmethod
+    def upload_handler(args):
         """
         usage: put <path1> [<path2>]
         Send file which is located in 'path1' to the server's 'path2'
@@ -240,9 +247,10 @@ class FTPClient:
         path2 = args[1] if len(args) > 1 else './'
         path2 = path2.rstrip('/')
         path2 = os.path.normpath('{}/{}'.format(path2, file_name))
-        self.upload_file(args[0], path2)
+        FTPClient.upload_file(args[0], path2)
 
-    def upload_file(self, path1, path2):
+    @staticmethod
+    def upload_file(path1, path2):
         """
         :param path1: local file's path
         :param path2: remote file's path
@@ -251,11 +259,13 @@ class FTPClient:
             with open(path1, 'rb') as file:
                 data = file.read()
         except:
-            self.eprint(sys.exc_info()[1])
+            FTPClient.eprint(sys.exc_info()[1])
         else:
-            self.ftp.upload_file(path2, data)
+            FTPClient.ftp.upload_file(path2, data)
 
-    def user_handler(self, args):
+    @get_func
+    @staticmethod
+    def user_handler(args):
         """
         usage: user <username>
         Send new user information
@@ -269,15 +279,19 @@ class FTPClient:
             print()
             print('Login failed')
         else:
-            self.ftp.login(username, password)
+            FTPClient.ftp.login(username, password)
 
-    def pwd_handler(self, args):
+    @get_func
+    @staticmethod
+    def pwd_handler(args):
         """
         Print current directory on remote machine
         """
-        self.ftp.get_location()
+        FTPClient.ftp.get_location()
 
-    def remove_handler(self, args):
+    @get_func
+    @staticmethod
+    def remove_handler(args):
         """
         usage: rm [-r] <path>
         Remove file on the remote machine
@@ -287,11 +301,13 @@ class FTPClient:
         if not args or (args[0] == '-r' and len(args) == 1):
             raise ValueError
         if args[0] == '-r':
-            self.ftp.remove_directory(args[1])
+            FTPClient.ftp.remove_directory(args[1])
         else:
-            self.ftp.remove_file(args[0])
+            FTPClient.ftp.remove_file(args[0])
 
-    def rename_handler(self, args):
+    @get_func
+    @staticmethod
+    def rename_handler(args):
         """
         usage: ren <file name>
         Rename file
@@ -299,27 +315,33 @@ class FTPClient:
         if len(args) != 2:
             raise ValueError
         else:
-            self.ftp.rename_file(args[0], args[1])
+            FTPClient.ftp.rename_file(args[0], args[1])
 
-    def cd_handler(self, args):
+    @get_func
+    @staticmethod
+    def cd_handler(args):
         """
         usage: cd <path>
         Change remote working directory
         """
         if not args:
             raise ValueError
-        self.ftp.change_directory(args[0])
+        FTPClient.ftp.change_directory(args[0])
 
-    def mkdir_handler(self, args):
+    @get_func
+    @staticmethod
+    def mkdir_handler(args):
         """
         usage: mkdir <directory name>
         Make directory on the remote machine
         """
         if not args:
             raise ValueError
-        self.ftp.make_directory(args[0])
+        FTPClient.ftp.make_directory(args[0])
 
-    def ls_handler(self, args):
+    @get_func
+    @staticmethod
+    def ls_handler(args):
         """
         usage: ls [-l] [<path>]
         Show content of remote directory
@@ -329,78 +351,122 @@ class FTPClient:
         if args:
             if args[0] == '-l':
                 if len(args) > 1:
-                    self.ftp.get_files(args[1])
+                    FTPClient.ftp.get_files(args[1])
                 else:
-                    self.ftp.get_files()
+                    FTPClient.ftp.get_files()
             else:
-                self.ftp.get_filenames(args[0])
+                FTPClient.ftp.get_filenames(args[0])
         else:
-            self.ftp.get_filenames()
+            FTPClient.ftp.get_filenames()
 
-    def size_handler(self, args):
+    @get_func
+    @staticmethod
+    def size_handler(args):
         """
         usage: size <file_name>
         Show size of remote file
         """
         if not args:
             raise ValueError
-        self.ftp.get_size(args[0])
+        FTPClient.ftp.get_size(args[0])
 
-    def debug_handler(self, args):
+    @get_func
+    @staticmethod
+    def debug_handler(args):
         """
         Toggle debugging mode
         """
-        if self.ftp.print_output:
+        if FTPClient.ftp.print_output:
             print('Debug is off')
         else:
             print('Debug is on')
-        self.ftp.print_output = not self.ftp.print_output
+        FTPClient.ftp.print_output = not FTPClient.ftp.print_output
 
-    def switch_mode_handler(self, args):
+    @get_func
+    @staticmethod
+    def switch_mode_handler(args):
         """
         Enter passive or active transfer mode
         """
-        self.ftp.passive_state = not self.ftp.passive_state
-        message = 'Passive mode is on' if self.ftp.passive_state else 'Active mode is on'
+        FTPClient.ftp.passive_state = not FTPClient.ftp.passive_state
+        message = 'Passive mode is on' if FTPClient.ftp.passive_state else 'Active mode is on'
         print(message)
 
-    def help_handler(self, args):
+    @get_func
+    @staticmethod
+    def help_handler(args):
         """
         usage: help [<command>]
         Print local help information about command. If command isn't specified show all available commands
         """
         if not args:
             print('Commands: ')
-            print('    '.join(filter(None, self.handlers.keys())))
+            print('    '.join(filter(None, FTPClient.handlers.keys())))
         else:
             command = args[0]
-            if command in self.handlers:
-                for i in filter(None, self.handlers[command].__doc__.split('\n')):
+            if command in FTPClient.handlers:
+                for i in filter(None, FTPClient.handlers[command].__doc__.split('\n')):
                     print(i.lstrip())
             else:
                 print('Unknown command "{}".'.format(command))
 
-    def exit_handler(self, args):
+    @get_func
+    @staticmethod
+    def exit_handler(args):
         """
         Terminate ftp session and exit
         """
         print('Goodbye')
-        self.ftp.close_connection()
+        FTPClient.ftp.close_connection()
         raise SystemExit
 
-    def unknown_command_handler(self, args):
+    @get_func
+    @staticmethod
+    def unknown_command_handler(args):
         """
         Handle unknown commands
         """
         print('Unknown command. Use "help"')
 
-    def eprint(self, *args, **kwargs):
+    @staticmethod
+    def eprint(*args, **kwargs):
         """
         Print message to the sys.stderr
         """
         print(*args, file=sys.stderr, **kwargs)
 
+    @staticmethod
+    def validate_path(path):
+        """
+        Return available directory's path by adding '*' symbol to the end
+        :param path: initial path
+        :return: validated path
+        """
+        while True:
+            if os.path.isdir(path):
+                path += '*'
+            else:
+                break
+        return path
+
+    handlers = {
+        'get': download_handler,
+        'put': upload_handler,
+        'user': user_handler,
+        'pwd': pwd_handler,
+        'rm': remove_handler,
+        'ren': rename_handler,
+        'cd': cd_handler,
+        'mkdir': mkdir_handler,
+        'ls': ls_handler,
+        'size': size_handler,
+        'debug': debug_handler,
+        'mode': switch_mode_handler,
+        'help': help_handler,
+        'exit': exit_handler,
+        None: unknown_command_handler
+    }
 
 if __name__ == '__main__':
-    f = FTPClient()
-    f.run()
+    FTPClient.parse_arguments()
+    FTPClient.run()
