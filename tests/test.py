ff65121001e4252a2da2844cf36339c19417a92a
Egor Druzhinin <egor.druzhinin97@gmail.com>
2018-11-29 19:26:39 +0500

    File downloading tests were added

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from time import sleep
+from unittest.mock import Mock, patch
 
 from src.ftp import FTP, WrongResponse
 from src.response import Response
@@ -16,6 +17,7 @@ class FTPTests(unittest.TestCase):
         self.message_patcher = patch.object(FTP, '_send_message',
                                             return_value=None)
         self.resp_patcher = patch.object(FTP, '_get_response')
+
         self.connect_patcher.start()
         self.message_patcher.start()
 
@@ -62,10 +64,10 @@ class FTPTests(unittest.TestCase):
         self.assertEqual(actual, '100 bytes')
 
     def test_list_files(self):
-        listing = """-rw-rw-r--   1 ftp      ftp       9967461 Dec 11  2007 01-Globus.mp3\r\n
+        listing = """-rw-rw-r--   1 ftp      ftp       9967461 08:00 01-Globus.mp3\r\n
 -rw-rw-r--   1 ftp      ftp       5525649 Dec 10  2007 02   Gospoda demokraty.mp3\r\n
 drw-rw-r--   1 ftp      ftp       8650309 Dec 11  2007 03 Byvshiy podjesaul.mp3\r\n
--rw-rw-r--   1 ftp      ftp      12458333 Dec 11  2007 07-Letniy dozhd.mp3\r\n
+-rw-rw-r--   1 ftp      ftp      12458333 15:32 07-Letniy dozhd.mp3\r\n
 drw-rw-r--   1 ftp      ftp       8245331 Dec 11  2007 11-Byvshiy podjesaul (1-y variant fonogrammy).mp3\r\n
         """
 
@@ -79,3 +81,50 @@ drw-rw-r--   1 ftp      ftp       8245331 Dec 11  2007 11-Byvshiy podjesaul (1-y
             ]
 
             self.assertEqual(self.ftp.list_files(), expected)
+
+    def test_file_downloading(self):
+        file_size = 100000
+        responses = [
+            Response(200, 'Type set to I'),
+            Response(213, str(file_size)),
+            Response(200, 'Type set to A'),
+            Response(200, 'Type set to I'),
+            Response(150, 'Opening BINARY mode data connection for file')
+        ]
+        self.resp_mock.side_effect = responses
+
+        with patch.object(self.ftp, '_open_data_connection', return_value=None):
+            data_mock = Mock()
+            mock = Mock()
+            data_mock.accept.return_value = (mock,)
+            mock.recv.side_effect = (b'#' for i in range(file_size))
+            self.ftp.data_socket = data_mock
+
+            actual = b''.join(self.ftp.get_file('test-file.txt'))
+            self.assertEqual(actual, b'#' * file_size)
+
+    def return_byte_with_delay(self, file_size, delay):
+        for i in range(file_size):
+            sleep(delay)
+            yield b'#'
+
+    def test_slow_file_downloading(self):
+        file_size = 3
+        responses = [
+            Response(200, 'Type set to I'),
+            Response(213, str(file_size)),
+            Response(200, 'Type set to A'),
+            Response(200, 'Type set to I'),
+            Response(150, 'Opening BINARY mode data connection for file')
+        ]
+        self.resp_mock.side_effect = responses
+
+        with patch.object(self.ftp, '_open_data_connection', return_value=None):
+            data_mock = Mock()
+            mock = Mock()
+            data_mock.accept.return_value = (mock,)
+            mock.recv.side_effect = self.return_byte_with_delay(file_size, 5)
+            self.ftp.data_socket = data_mock
+
+            actual = b''.join(self.ftp.get_file('test-file.txt'))
+            self.assertEqual(actual, b'#' * file_size)
